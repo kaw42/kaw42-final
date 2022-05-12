55e1af350081662fd0459c9c626097ca5f17b6f3
kaw42 <kaw42@njit.edu>
2022-05-11 22:10:16 -0400

    fixed user test

--- tests/user_test.py
+++ tests/user_test.py
@@ -24,14 +24,14 @@ def test_adding_user(application):
         #asserting that the user retrieved is correct
         assert user.email == 'kaw42@njit.edu'
         #this is how you get a related record ready for insert
-        user.transactions= [Transactions("200","Debit"),Transactions("300","Credit")]
+        user.transactions= [Transactions(200.0,"Debit"),Transactions(300.0,"Credit")]
         #commit is what saves the songs
         db.session.commit()
         assert db.session.query(Transactions).count() == 2
         transaction1 = Transactions.query.filter_by(account_type='Debit').first()
-        assert transaction1.amount == "200"
+        assert transaction1.amount == 200.0
         db.session.commit()
-        transaction2 = Transactions.query.filter_by(amount='300').first()
+        transaction2 = Transactions.query.filter_by(amount=300.0).first()
         assert transaction2.account_type == "Credit"
         #checking cascade delete
         db.session.delete(user)
